3a5f680d41cd2aa00b91704eba04b4f491bc1172
gato roboto <omerinyerix@gmail.com>
2025-06-28 19:06:03 +0300

    Add project files.

---
+++ gatototbalatroto/Creator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gatototbalatroto
+{
+    internal class Creator
+    {
+        public Dictionary<string, List<string>> Create()
+        {
+
+
+            Dictionary<string, List<string>> all = new Dictionary<string, List<string>>();
+
+            string categoryName = "";
+            string input;
+
+            List<string> categoriesToAdd = new List<string>();
+
+            Boolean x = true;
+            while (x)
+            {
+                Console.Write(
+                    "kategori ismi eklemek veya degistirmek icin 1 " +
+                    "item eklemek icin 2 " +
+                    "cikmak icin 0 " +
+                    ": "
+                    );
+
+                input = Console.ReadLine();
+
+                if (input == "1")
+                {
+
+                    Console.Write("kategori ismi giriniz ");
+                    categoryName = Console.ReadLine();
+
+                }
+
+                else if (input == "2")
+                {
+
+                    Console.WriteLine("eklenecek item adi giriniz ");
+                    categoriesToAdd.Add(Console.ReadLine());
+
+                }
+                else if (input == "0")
+                {
+
+                    x = false;
+
+                }
+            }
+            all.Add(categoryName, categoriesToAdd);
+            return all;
+        }
+    }
+}
---
+++ gatototbalatroto/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace gatototbalatroto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            bool isActive = true;
+            Creator creator = new Creator();
+            while (isActive)
+            {
+
+                Dictionary<string, List<string>> all = new Dictionary<string, List<string>>();
+
+                Console.WriteLine("ne yapmak istiyorsun?");
+                Console.WriteLine("kategori olusturmak icin 1");
+                Console.WriteLine("kategorileri gormek icin 2");
+                Console.WriteLine("bitirmek icin 0 ");
+
+                input = Console.ReadLine();
+
+                if (input == "1")
+                {
+                    //List<string> categoryNameFromCreator = creator.Create().Keys.ToList();
+                    //string categoryName = categoryNameFromCreator[0];
+
+                    Dictionary<string, List<string>> dictionaryComesFromCreator = new Dictionary<string, List<string>>(creator.Create());
+
+                    Console.WriteLine("olusturulan kategori: ");
+
+
+
+                    string categoryName = dictionaryComesFromCreator.Keys.First();//kategori adi
+                    Console.Write("eklenen kategori adi: ");
+                    Console.WriteLine(categoryName.ToUpper());
+
+                    List<string> categoryItems = dictionaryComesFromCreator[categoryName];//kategori itemleri
+
+                    Console.WriteLine("eklenen kategori itemleri: ");
+                    Console.WriteLine("********************");
+                    foreach (string item in categoryItems)
+                    {
+                        Console.WriteLine(item.ToLower());
+                    }
+                }
+
+                else if (input == "2")
+                {
+                    List<string> categoriesList = all.Keys.ToList();
+                    foreach (string item in categoriesList)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+            }
+        }
+    }
+}
